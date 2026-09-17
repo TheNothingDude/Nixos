41bd683c281d6cb14da75d79d51c7bf4e568b9a9
Helium <aaron.assassin.9.0@gmail.com>
2026-09-17 18:40:12 +0200

    auto: system update 2026-09-17_18-40-12

--- configuration.nix
+++ configuration.nix
@@ -1,59 +1,30 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { inputs, config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
 
-  # Use the systemd-boot EFI boot loader.
+  # --- Bootloader (GRUB) ---
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
   
   boot.loader.grub = {
-  enable = true;
-  device = "nodev";
-  efiSupport = true; # Set to false if using Legacy BIOS
-  useOSProber = true;
-  configurationLimit = 2; 
-};
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  #services
-  systemd = {
-  user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-   };
+    enable = true;
+    device = "nodev";
+    efiSupport = true; # Set to false if using Legacy BIOS
+    useOSProber = true;
+    configurationLimit = 2; 
   };
-  # Enable networking
+
+  # --- Networking ---
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # --- Time & Locale ---
   time.timeZone = "Europe/Budapest";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "hu_HU.UTF-8";
     LC_IDENTIFICATION = "hu_HU.UTF-8";
@@ -65,25 +36,25 @@
     LC_TELEPHONE = "hu_HU.UTF-8";
     LC_TIME = "hu_HU.UTF-8";
   };
- 
-  #X stuff
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = ["amdgpu"];
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  
-  #Niri
-  services.displayManager.defaultSession = "niri";
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+
+  # --- Display, Audio & Hardware ---
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "amdgpu" ];
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
-  # Enable CUPS o print documents.
+  services.displayManager = {
+    gdm.enable = true;
+    defaultSession = "niri";
+  };
+
   services.printing.enable = false;
 
-  # Enable sound with pipewire.
+  # Audio via PipeWire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -91,146 +62,99 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # Use the WirePlumber session manager
-    #wireplumber.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  #kernels
-  nixpkgs.overlays = [
-     (final: prev: {
-    steam = prev.steam.override {
-      extraArgs = "-cef-disable-gpu-compositing";
-    };
-  })
-    inputs.nix-cachyos-kernel.overlays.pinned
-  ];
-  # Choose your preferred CachyOS kernel package variant
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-
-  users.users."helium" = {
-    isNormalUser = true;
-    description = "Helium";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-     vim
-    ];
-  };
-  #home manager
-  home-manager = {
-  # also pass inputs to home-manager modules
-  backupFileExtension = "backup";
-  extraSpecialArgs = {inherit inputs;};
-  users = {
-    "helium" = import ./home.nix;
-  };
-};
-  # fonts
-  fonts.packages = with pkgs; [
-  nerd-fonts.fira-code
-  nerd-fonts.annotationmono
-  nerd-fonts.jetbrains-mono
-  ];
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.steam.enable = true;
-  programs.niri.enable = true;
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true; 
-  };
-  programs.xwayland.enable = true;
-  programs.git.enable = true;
-  services.flatpak.enable = true;
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  #gaymin uwu
 
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
   hardware.amdgpu.initrd.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
 
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-      "\${HOME}/.steam/root/compatibilitytools.d";
-    SDL_VIDEODRIVER = "wayland";
-    DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_8_0}";
+  # --- Kernel & Overlays ---
+  nixpkgs.overlays = [
+    (final: prev: {
+      steam = prev.steam.override {
+        extraArgs = "-cef-disable-gpu-compositing";
+      };
+    })
+    inputs.nix-cachyos-kernel.overlays.pinned
+  ];
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+
+  # --- Users ---
+  users.users."helium" = {
+    isNormalUser = true;
+    description = "Helium";
+    extraGroups = [ "networkmanager" "wheel" ];
   };
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-   environment.systemPackages = with pkgs; [
-      inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-      xwayland-satellite
-      fastfetch
-      mangohud
-      protonplus
-      vim
-      git
-      gh
-      unzip
-      unrar
-   ];
 
-  #garbage collection
-   nix.settings = {
-      auto-optimise-store = true; # Automatically hardlinks identical files in the nix store to save disk space
-      experimental-features = [ "nix-command" "flakes" ];
+  # --- Home Manager ---
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "helium" = import ./home.nix;
     };
+  };
+
+  # --- Fonts ---
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.annotationmono
+    nerd-fonts.jetbrains-mono
+  ];
+
+  # --- Programs & Features ---
+  nixpkgs.config.allowUnfree = true;
+
+  programs.firefox.enable = true;
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+  programs.gamemode.enable = true;
+  programs.niri.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+  programs.xwayland.enable = true;
+  programs.git.enable = true;
+  services.flatpak.enable = true;
+
+  # --- Environment Variables ---
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    SDL_VIDEODRIVER = "wayland";
+  };
+
+  # --- System Packages (CLI tools only) ---
+  # GUI apps have been moved to home.nix
+  environment.systemPackages = with pkgs; [
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    xwayland-satellite
+    mangohud
+    protonplus
+    fastfetch
+    vim
+    git
+    gh
+    unzip
+    unrar
+  ];
+
+  # --- Nix Settings & Garbage Collection ---
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
-
+  # --- State Version ---
+  system.stateVersion = "26.05";
 }
